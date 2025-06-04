d5b8f65759649f09772b2a4325dc299a00fc8b69
acardoco <andres.cardosoc12@gmail.com>
2025-06-04 22:55:05 +0200

    updated pvcs keycloak to longhorn

--- kubernetes/keycloak/deploy-keycloak.sh
+++ kubernetes/keycloak/deploy-keycloak.sh
@@ -1,4 +1,5 @@
 source .env
+envsubst < 00-pvc.yaml | kubectl apply -f -
 envsubst < 01-secret.yaml | kubectl apply -f -
 envsubst < 02-deployment.yaml | kubectl apply -f -
 envsubst <  03-service.yaml | kubectl apply -f -
