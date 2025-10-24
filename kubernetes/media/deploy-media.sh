f8bfc64a94cd5d77039bfb7b7d9396316619ff20
acardoco <andres.cardosoc12@gmail.com>
2025-10-24 21:55:39 +0200

    radarr up

--- kubernetes/media/deploy-media.sh
+++ kubernetes/media/deploy-media.sh
@@ -12,3 +12,14 @@ envsubst < sonarr/01-pvc-sonarr.yaml | kubectl apply -f -
 envsubst < jellyfin/05-deployment-jellyfin.yaml | kubectl apply -f -
 envsubst < sonarr/05-deployment-sonarr.yaml | kubectl apply -f -
 envsubst < radarr/05-deployment-radarr.yaml | kubectl apply -f -
+envsubst < jackett/05-deployment-jackett.yaml | kubectl apply -f -
+envsubst < qBittorrent/05-deployment-qBittorrent.yaml | kubectl apply -f -
+
+
+envsubst < jellyfin/06-svc-jellyfin.yaml | kubectl apply -f -
+envsubst < sonarr/06-svc-sonarr.yaml | kubectl apply -f -
+envsubst < radarr/06-svc-radarr.yaml | kubectl apply -f -
+envsubst < jackett/06-svc-jackett.yaml | kubectl apply -f -
+envsubst < qBittorrent/06-svc-qBittorrent.yaml | kubectl apply -f -
+
+envsubst < default-headers-media.yaml | kubectl apply -f -
--- kubernetes/media/plex/READme.md
+++
@@ -1,7 +0,0 @@
-kubectl create namespace media
-
-kubectl label nodes andres-worker media=plex --overwrite
-
-kubectl -n media create secret generic plex-claim --from-literal=PLEX_CLAIM=claim-XXXXXXXX
-
-## TODO deploy
