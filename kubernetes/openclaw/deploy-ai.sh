4ab94095529338088686601b6a1f343e649aa21f
Andrés Cardoso <andres.cardosoc12@gmail.com>
2026-03-20 18:14:23 +0100

    cicd in github for openclaw image

--- kubernetes/openclaw/deploy-ai.sh
+++ kubernetes/openclaw/deploy-ai.sh
@@ -1,8 +1,10 @@
+source .env
+
 kubectl apply -f 00-namespace.yaml
 kubectl apply -f 00-quota.yaml
 kubectl apply -f 01-ollama.yaml
 kubectl apply -f 02-rbac-media.yaml
 kubectl apply -f 02-rbac-monitoring.yaml
-# kubectl apply -f 03-openclaw-secret.yaml
+envsubst < 03-openclaw-secret.yaml | kubectl apply -f -
 # kubectl apply -f 04-openclaw-config.yaml
 # kubectl apply -f 05-openclaw.yaml
